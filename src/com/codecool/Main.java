b15534bfdb9e9f3b12d0ba9c3034443e3a69c314
kovacsgabo <kovacsgaboo@gmail.com>
2018-01-22 14:08:36 +0100

    starting implement

--- src/com/codecool/Main.java
+++ src/com/codecool/Main.java
@@ -1,1 +1,8 @@
 package com.codecool;
+
+public class Main{
+    public static void main(String[] args){
+        Simulation valami = new Simulation(1,2);
+        valami.generateDate();
+    }
+}
--- src/com/codecool/Simulation.java
+++ src/com/codecool/Simulation.java
@@ -1,1 +1,24 @@
 package com.codecool;
+
+import java.util.Random;
+
+public class Simulation{
+
+    int firstDie, secondDie;
+
+    public Simulation(int firstDie, int secondDie){
+        this.firstDie = firstDie;
+        this.secondDie = secondDie;
+    }
+
+    public void generateDate(){
+        Random randomDice = new Random();
+        int firstDie = randomDice.nextInt(6) + 1;
+        int secondDie = randomDice.nextInt(6) + 1;
+        int result = firstDie + secondDie;
+        /*System.out.println(firstDie);
+        System.out.println(secondDie);
+        System.out.println(result);*/
+    }
+    
+}
