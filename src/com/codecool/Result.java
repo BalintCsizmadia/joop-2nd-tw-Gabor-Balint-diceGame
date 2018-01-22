c4158a029a9298bccffc5a94a52e9047d241380b
Bálint Csizmadia <balintcsizmadia1@gmail.com>
2018-01-22 15:07:21 +0100

    write to file in progress

--- src/com/codecool/Result.java
+++ src/com/codecool/Result.java
@@ -1,1 +1,3 @@
 package com.codecool;
+
+// getStatistics()
--- src/com/codecool/Simulation.java
+++ src/com/codecool/Simulation.java
@@ -2,6 +2,15 @@ package com.codecool;
 
 import java.util.Random;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+import java.util.*;
 public class Simulation{
 
     int firstDie, secondDie;
@@ -10,7 +19,7 @@ public class Simulation{
         this.firstDie = firstDie;
         this.secondDie = secondDie;
     }
-
+    
     public void generateDate(){
         Random randomDice = new Random();
         int firstDie = randomDice.nextInt(6) + 1;
@@ -19,6 +28,19 @@ public class Simulation{
         /*System.out.println(firstDie);
         System.out.println(secondDie);
         System.out.println(result);*/
+        String res = "" + result;
+
+
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter("com/codecool/results.csv", true));
+            StringBuilder sb = new StringBuilder();
+            bw.write (res);
+            bw.append(",");
+            bw.close();
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+        }
     }
+
     
 }
