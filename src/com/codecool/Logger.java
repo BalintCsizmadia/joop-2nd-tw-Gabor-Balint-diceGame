d4a849c6d05c8391322fb1c7e46489bc5a74e052
kovacsgabo <kovacsgaboo@gmail.com>
2018-01-22 15:08:37 +0100

    starting logger

--- src/com/codecool/Logger.java
+++ src/com/codecool/Logger.java
@@ -1,1 +1,16 @@
 package com.codecool;
+
+public class Logger{
+    String type;
+    String message;
+
+    public Logger(String type, String message){
+        this.type = type;
+        this.message = message;
+
+    }
+
+    public String log(String type, String message){
+        return message;
+    }
+}
