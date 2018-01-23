4a902c605152c4e2878de7e1d296687c259d92bb
kovacsgabo <kovacsgaboo@gmail.com>
2018-01-23 15:58:04 +0100

    logic in progress

--- src/com/codecool/Simulator.java
+++ src/com/codecool/Simulator.java
@@ -13,6 +13,27 @@ public class Simulator {
     public void run() {
         // code
         simulation.generateData();
+        simulation.load();
+        logger.log("run", "adatok feldolgozva és visszatoltve");
+        int wins = 0;
+        for(int i = 0 ; i < simulation.intArray.length; i++){
+            
+            int x = simulation.intArray[i];
+            if (x == 7 || x == 11) wins++;
+            if (x == 2 || x == 3 || x == 12){
+                break;
+            }
+            else {
+                for(int j = 0; j < simulation.intArray.length; j++){
+                int y = simulation.intArray[i+j];
+                if (y == 7) break;
+                if (y == x) wins++;
+                }
+            } 
+        }
+        System.out.println(wins);
         
     }
+        
+        
 }
