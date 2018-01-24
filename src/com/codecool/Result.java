6b0f0dc5cc9402e74788389fee9f0dc0b78e28bc
Bálint Csizmadia <balintcsizmadia1@gmail.com>
2018-01-24 08:49:53 +0100

    working on Result and Statistics

--- src/com/codecool/Result.java
+++ src/com/codecool/Result.java
@@ -1,3 +1,36 @@
 package com.codecool;
 
-// getStatistics()
+public class Result {
+
+    int snakeEye;
+    int aceDeuce;
+    int yo;
+    int boxCars;
+    int hiLo;
+    int anyCraps;
+    int anySeven;
+    int allGame;
+
+    public Result(int snakeEye, int aceDeuce, int yo, int boxCars, int hiLo, int anyCraps, int anySeven, int allGame) {
+        this.snakeEye = snakeEye;
+        this.aceDeuce = aceDeuce;
+        this.yo = yo;
+        this.boxCars = boxCars;
+        this.hiLo = hiLo;
+        this.anyCraps = anyCraps;
+        this.anySeven = anySeven;
+        this.allGame = allGame;
+    }
+
+    public String getStatistics() {
+        return "Snake eye: " + snakeEye + " wins from " + allGame + " games.\n" + 
+               "Ace-deuce: " + aceDeuce + " wins from " + allGame + " games.\n" + 
+               "Yo: " + yo + " wins from " + allGame + " games.\n" +
+               "Boxcars: " + boxCars + " wins from " + allGame + " games.\n" +
+               "Hi-lo: " + hiLo + " wins from " + allGame + " games.\n" +
+               "Any craps: " + anyCraps + " wins from " + allGame + " games.\n" + 
+               "Any seven: " + anySeven + " wins from "  + allGame + " games.";
+
+
+    }
+}
